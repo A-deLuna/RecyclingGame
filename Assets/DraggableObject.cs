79527086b73d356929c6d8656f3eccbeae98360b
Antonio de Luna <tonydelun@gmail.com>
2016-05-21 14:43:10 -0700

    added touch controls

--- Assets/DraggableObject.cs
+++ Assets/DraggableObject.cs
@@ -2,23 +2,31 @@
 using System.Collections;
 
 public class DraggableObject : MonoBehaviour {
-	public float speed = 0.5f;
+	public float speed = 0.1f;
+	public float timeDiff = 1f;
 	// Use this for initialization
+	private bool touchEnabled = true;
+	private float timeToEnable = 0.0f;
+
 	void Start () {
 	
 	}
 	
 	// Update is called once per frame	
-	void Update () {
-		if (Input.GetMouseButton (0)) {
-			float y = Input.GetAxis ("Mouse Y");
-			float x = Input.GetAxis ("Mouse X");
-			transform.Translate (new Vector3(x * speed, y * speed));
-	
+	void OnMouseDrag () {
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved && touchEnabled) {
+			// Get movement of the finger since last frame
+			// Move object across XY plane
+			Vector3 pos = Camera.main.ScreenToWorldPoint (new Vector3 (Input.GetTouch(0).position.x, Input.GetTouch(0).position.y, 5));
+			transform.position = new Vector3(pos.x, pos.y , 0);
 		}
 	}
 
-
+	void Update() {
+		if (timeToEnable < Time.time) {
+			touchEnabled = true;
+		}
+	}
 
 	void OnCollisionEnter2D(Collision2D other) {
 		Debug.Log ("Collision");
@@ -28,5 +36,7 @@ public class DraggableObject : MonoBehaviour {
 		Debug.Log ("Trigger");
 		ScoreManager.score++;
 		transform.position = new Vector3(0,0,0);
+		touchEnabled = false;
+		timeToEnable = Time.time + timeDiff;
 	}
 }
