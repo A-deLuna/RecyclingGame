377f2f19722dc980e4dfcaf65d6ee3a23059fdba
Antonio de Luna <tonydelun@gmail.com>
2016-05-21 12:29:29 -0700

    first commit

---
+++ Assets/DraggableObject.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableObject : MonoBehaviour {
+	public float speed = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame	
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			float y = Input.GetAxis ("Mouse Y");
+			float x = Input.GetAxis ("Mouse X");
+			transform.Translate (new Vector3(x * speed, y * speed));
+	
+		}
+	}
+
+
+
+	void OnCollisionEnter2D(Collision2D other) {
+		Debug.Log ("Collision");
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Trigger");
+		ScoreManager.score++;
+		transform.position = new Vector3(0,0,0);
+	}
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour {
+
+	// Use this for initialization
+	public static int score;
+	Text text;
+	void Awake () {
+		text = GetComponent<Text> ();
+		score = 0;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		text.text = "Score: " + score;
+	}
+}
