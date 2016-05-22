7976f7b639fdb34a99c87428d7b08c4ce4188004
Antonio de Luna <tonydelun@gmail.com>
2016-05-22 12:39:01 -0700

    agregar imagenes determinar el tipo de imagen, y resize

--- Assets/DraggableObject.cs
+++ Assets/DraggableObject.cs
@@ -1,18 +1,26 @@
-﻿using UnityEngine;
+﻿	using UnityEngine;
 using System.Collections;
+
 
 public class DraggableObject : MonoBehaviour
 {
-    public float speed = 0.1f;
+	enum Color {
+		BLUE,
+		BROWN,
+		GREEN,
+		BLACK
+	}
+	public float speed = 0.1f;
     public float timeDiff = 1f;
     // Use this for initialization
     private bool touchEnabled = true;
     private float timeToEnable = 0.0f;
-    public int tipo = 0;
+	public Sprite[] sprites;
+	private Color tipo;
 
     void Start()
     {
-
+		GenerateNextObject ();
     }
 
     // Update is called once per frame	
@@ -40,62 +48,81 @@ public class DraggableObject : MonoBehaviour
     {
         Debug.Log("Collision");
     }
-
+		
     void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log("Trigger");
-        ScoreManager.score++;
-        transform.position = new Vector3(0, 0, 0);
-        touchEnabled = false;
-        timeToEnable = Time.time + timeDiff;
-        if (other.gameObject.tag == "Blue")
-        {
-            if (tipo == 1)
-            {
-                Destroy(other.gameObject);
-                correct();
-               
-            }
-            else
-                incorrect();
+    {      
+		Debug.Log ("other tag: " + other.gameObject.tag + " State: " + tipo);
+		if (other.gameObject.tag == "Blue" && tipo == Color.BLUE)
+        {           
+        	correct();                     
         }
-        if (other.gameObject.tag == "Brown")
+		if (other.gameObject.tag == "Brown" && tipo == Color.BROWN)
         {
-            if (tipo == 2)
-            {
-                Destroy(other.gameObject);
-                correct();
-          
-            }
-            else
-                incorrect();
+            correct();   
         }
-        if (other.gameObject.tag == "Green")
+		if (other.gameObject.tag == "Green" && tipo == Color.GREEN)
+		{
+			correct ();
+		} 
+		if (other.gameObject.tag == "Black" && tipo == Color.BLACK)
         {
-            if (tipo == 3)
-            {
-                Destroy(other.gameObject);
-                incorrect();
-            }
-            else
-                incorrect();
+            correct();
         }
-        if (other.gameObject.tag == "Black")
-        {
-            if (tipo == 4)
-            {
-                Destroy(other.gameObject);
-                correct();
-            }
-            else
-                incorrect();
-        }
+		incorrect ();
+   		GenerateNextObject ();
     }
+
+	void GenerateNextObject() {
+		int next = Random.Range (0, 20);
+		Debug.Log (next);
+		GetComponent<SpriteRenderer> ().sprite = sprites [next];
+		ResizeSprite ();
+		SetState (next);
+		ResetPosition ();
+	}
+
+	void SetState(int index) {
+		Debug.Log(index/5);
+		switch (index / 5) {
+		case 0:
+			tipo = Color.BLUE;
+			break;
+		case 1:
+			tipo = Color.BROWN;
+			break;
+		case 2:
+			tipo = Color.GREEN;
+			break;
+		case 3:
+			tipo = Color.BLACK;
+			break;
+		}
+	}
+	void ResizeSprite() {
+		transform.localScale = Vector3.one;
+
+		float width = GetComponent<SpriteRenderer> ().sprite.bounds.size.x;
+		float height = GetComponent<SpriteRenderer> ().sprite.bounds.size.y;
+
+		transform.localScale = new Vector2 (3f / width, 3f / height);
+		GetComponent<BoxCollider2D> ().size = new Vector2 (width, height);
+	}
+
+	void ResetPosition() {
+		transform.position = new Vector3(0, 0, 0);
+		DisableTouch ();
+	}
+
+	void DisableTouch() {
+		touchEnabled = false;
+		timeToEnable = Time.time + timeDiff;
+	}
 
     void correct()
     {
-
+		ScoreManager.score++;
     }
+
     void incorrect()
     {
 
